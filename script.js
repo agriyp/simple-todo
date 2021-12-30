e42e29b6fafa054daa7ed87b8aa6dfbf81349793
agriyp <agriyp@gmail.com>
2021-12-30 16:47:21 +0700

    menambahkan fungsi checked

--- script.js
+++ script.js
@@ -48,8 +48,14 @@ function deleteCheck(e){
     // delete todo
     if(item.classList[0] === "trash-btn"){
         const todo = item.parentElement;
-        todo.remove();
-    } else if(item.classList[0] === "complete-btn"){
+        // animasi
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+    
+    if(item.classList[0] === "complete-btn"){
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
